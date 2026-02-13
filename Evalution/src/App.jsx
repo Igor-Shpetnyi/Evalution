88507cc4302af4dbdde10f9183cecd77c1a56f85
igor.shpetnyi@gmail.com <igor.shpetnyi@gmail.com>
2026-02-13 15:02:07 +0200

    initial commit

---
+++ Evalution/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>🚀 My first Vercel deploy</h1>
+      <p>If you see this online — deploy works!</p>
+    </div>
+  );
+}
+
+export default App;
